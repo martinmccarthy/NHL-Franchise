dd17799a03e9f2a6b4f2e7aa9541c65fa83bd945
Martin McCarthy <martinmccarthy@knights.ucf.edu>
2023-03-07 17:42:06 -0500

    implements adding season dates for each team

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
+/*  This will 100000% need to be split up into different components and functions,
+    I am lazy though so this will do for now. */
+
 import './App.css';
 import {useState} from 'react';
 import axios from 'axios';
-import teams from "@nhl-api/teams";
+import teams, { getTeamId } from "@nhl-api/teams";
 
 import jsonData from './Roster_JSON/teams.json'
 
 function App() {
   const [rosterLoaded, setRosterLoaded] = useState(false);
-  const [roster, setRoster] = useState([]);
+  
   const [forwards, setForwards] = useState();
-  const [forwardsActive, setForwardsActive] = useState(false);
+  const [defense, setDefense] = useState();
+  const[goalie, setGoalie] = useState();
+  
+  const[myTeam, setMyTeam] = useState();
   function getLogos() {
     var currentTeams = teams.filter(team => team.isActive === true);
 
@@ -28,24 +34,67 @@ function App() {
     return currentTeams;
   }
 
-  function getRoster(team) {
+  function getSchedule() {
+    var teamID;
+    if(myTeam !== 'kraken') {
+      teamID = getTeamId(myTeam);
+    }
+    else {
+      teamID = 55;
+    }
+
+    /*  There's a very specific case here where the NHL scheduled games for the predators and sharks before the season
+        started, because of this, we need to have a start date variable which changes if the team is them since the rest
+        of the teams potentially have preseason games at that time. */
+    var startDate;
+    if(myTeam === 'sharks' || myTeam === 'predators') {
+      startDate = '2022-10-07';
+    }
+    else {
+      startDate = '2022-10-11';
+    }
+    axios.get(`https://statsapi.web.nhl.com/api/v1/schedule?teamId=${teamID}&startDate=${startDate}&endDate=2023-04-14`).then( res => {
+      console.log(res.data.dates); 
+    })
+  }
+
+  function getRoster(team, whoseTeam) {
     let teamArray = team.name.toLowerCase().split(' ');
     let teamName = teamArray[teamArray.length - 1];
     let currentRoster = jsonData[teamName];
-    console.log(currentRoster.forward);
-    delete currentRoster.forward[currentRoster.forward.length - 1]
-    setForwards(currentRoster.forward);
-    setForwardsActive(true);
+    setMyTeam(teamName);
+    if(whoseTeam === 'myteam') {
+      delete currentRoster.forward[currentRoster.forward.length - 1];
+      setForwards(currentRoster.forward);
+      
+      delete currentRoster.defense[currentRoster.defense.length - 1];
+      setDefense(currentRoster.defense);
+      
+      delete currentRoster.goalie[currentRoster.goalie.length - 1];
+      setGoalie(currentRoster.goalie);
+  
+      setRosterLoaded(true);
+    }
+  }
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  function averageCaphits(capHits) {
+    return Math.round(capHits.reduce((prev, curr) => prev + curr) / capHits.length)
+  }
+
+  function simSeason() {
+
   }
 
   function parseCapHit(player) {
     if(!player) return;
     let yearsLeft = Number(player[1].split(' ')[0]);
     var caphits = []
-    console.log('for player: ', player[0]);
 
     for(var i = 8; i < (yearsLeft + 8); i++) {
-      console.log(player[i])
       if(player[i] === undefined) {
         caphits.push(caphits[caphits.length - 1]);
       }
@@ -55,10 +104,7 @@ function App() {
       }
     }
 
-    console.log(caphits);
-    const average = list => list.reduce((prev, curr) => prev + curr) / list.length;
-
-    return average(caphits);
+    return numberWithCommas(averageCaphits(caphits));
   }
 
   return (
@@ -66,47 +112,76 @@ function App() {
       {!rosterLoaded && <div className="logos">
         <h1>Pick your franchise!</h1>
         {getLogos().map(team => (
-          <img onClick={() => getRoster(team)} width="100px" key={team.id} src={team.logo} alt={team.name}/>
+          <img onClick={() => getRoster(team, 'myteam')} width="100px" key={team.id} src={team.logo} alt={team.name}/>
         ))}
       </div>}
-      {forwardsActive && (
-        <div className="forwards">
-          <h1>Forwards</h1>
-          <table>
-            <tr>
-              <th>Player</th>
-              <th>Position</th>
-              <th>Cap Hit</th>
-            </tr>
-            {Object.keys(forwards).map(player => (
-              <tr>
-                <td>{forwards[player][0]}</td>
-                <td>{forwards[player][3]}</td>
-                <td>{parseCapHit(forwards[player])}</td>
-              </tr>
+
+      {rosterLoaded && (
+        <div className="loaded">
+          <div className="myTeamPlayers">
+            <div className="forwards">
+              <h1>Forwards</h1>
+              <table className='table'>
+                <tr>
+                  <th>Player</th>
+                  <th>Position</th>
+                  <th>Cap Hit</th>
+                </tr>
+                {Object.keys(forwards).map(player => (
+                  <tr>
+                    <td>{forwards[player][0]}</td>
+                    <td>{forwards[player][3]}</td>
+                    <td>{parseCapHit(forwards[player])}</td>
+                  </tr>
+                ))}
+              </table>
+            </div>
+            <div className="defense">
+              <h1>Defense</h1>
+              <table className='table'>
+                <tr>
+                  <th>Player</th>
+                  <th>Position</th>
+                  <th>Cap Hit</th>
+                </tr>
+                {Object.keys(defense).map(player => (
+                  <tr>
+                    <td>{defense[player][0]}</td>
+                    <td>{defense[player][3]}</td>
+                    <td>{parseCapHit(defense[player])}</td>
+                  </tr>
+                ))}
+              </table>
+            </div>
+            <div className="goalie">
+              <h1>Goalies</h1>
+              <table className='table'>
+                <tr>
+                  <th>Player</th>
+                  <th>Position</th>
+                  <th>Cap Hit</th>
+                </tr>
+                {Object.keys(goalie).map(player => (
+                  <tr>
+                    <td>{goalie[player][0]}</td>
+                    <td>{goalie[player][3]}</td>
+                    <td>{parseCapHit(goalie[player])}</td>
+                  </tr>
+                ))}
+              </table>
+            </div>
+          </div>
+          {/* <div className="tradingTeam">
+            <h1>Pick a team to trade with</h1>
+            {getLogos().map(team => (
+              <img onClick={() => getRoster(team, 'myteam')} width="100px" key={team.id} src={team.logo} alt={team.name}/>
             ))}
-          </table>
+          </div> */}
+          <button onClick={getSchedule}>
+            Simulate a season
+          </button>
         </div>
       )}
-
-      {/* {rosterLoaded && (
-        <div>
-          <table>
-            <tr>
-              <th>Player</th>
-              <th>Position</th>
-              <th>Number</th>
-            </tr>
-            {roster.map(player => (
-              <tr>
-                <td>{player.person.fullName}</td>
-                <td>{player.position.name}</td>
-                <td>{player.jerseyNumber}</td>
-              </tr>
-            ))}
-          </table>
-        </div>
-      )} */}
     </div>
   );
 }
